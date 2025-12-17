2dd5b3c30d65706c42b2e3772a57a5810e897642
beingbiplov <sharmabiplov@gmail.com>
2025-12-17 14:28:18 +0100

    fix: terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "cert-verification-tf-state"
-    key          = "phase1/s3/terraform.tfstate"
-    region       = "eu-west-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket  = "cert-verification-tf-state"
+    key     = "phase1/s3/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
   }
 }
