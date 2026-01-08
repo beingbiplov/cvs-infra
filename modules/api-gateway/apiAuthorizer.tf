1b48c20b616bc7251ac4abe26ed285714718578b
beingbiplov <sharmabiplov@gmail.com>
2026-01-08 17:09:59 +0100

    fix: update cognito user pool arn

--- modules/api-gateway/apiAuthorizer.tf
+++ modules/api-gateway/apiAuthorizer.tf
@@ -1,11 +1,11 @@
 resource "aws_api_gateway_authorizer" "cognito" {
   name            = "cert-verification-cognito-authorizer"
-  rest_api_id     = aws_api_gateway_rest_api.this.id
+  rest_api_id     = aws_api_gateway_rest_api.certificate_api.id
   type            = "COGNITO_USER_POOLS"
 
   identity_source = "method.request.header.Authorization"
 
   provider_arns = [
-    module.cognito.user_pool_arn
+    var.cognito_user_pool_arn
   ]
 }
