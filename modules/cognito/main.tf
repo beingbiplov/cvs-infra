8b5661881dff963c8db5bb7b8a0dfcd045efa939
beingbiplov <sharmabiplov@gmail.com>
2026-01-08 21:16:50 +0100

    fix: remove user pool client

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -39,27 +39,3 @@ resource "aws_cognito_user_pool" "this" {
   }
 }
 
-resource "aws_cognito_user_pool_client" "this" {
-  name         = "${var.project_name}-${var.environment}-web-client"
-  user_pool_id = aws_cognito_user_pool.this.id
-
-  generate_secret = false 
-
-  explicit_auth_flows = [
-    "ALLOW_USER_PASSWORD_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH",
-    "ALLOW_ADMIN_USER_PASSWORD_AUTH"
-  ]
-
-  prevent_user_existence_errors = "ENABLED"
-
-  access_token_validity  = 60
-  id_token_validity      = 60
-  refresh_token_validity = 30
-
-  token_validity_units {
-    access_token  = "minutes"
-    id_token      = "minutes"
-    refresh_token = "days"
-  }
-}
