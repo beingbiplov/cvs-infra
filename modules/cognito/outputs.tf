edff55b9e96cfade8e1727f60ae90cd6e0e24214
beingbiplov <sharmabiplov@gmail.com>
2026-01-08 21:17:06 +0100

    fix: update cognito outputs

--- modules/cognito/outputs.tf
+++ modules/cognito/outputs.tf
@@ -7,27 +7,11 @@ output "user_pool_arn" {
 }
 
 output "user_pool_client_id" {
-  value = aws_cognito_user_pool_client.this.id
-}
-
-output "user_pool_id" {
-  value = aws_cognito_user_pool.this.id
-}
-
-output "user_pool_arn" {
-  value = aws_cognito_user_pool.this.arn
-}
-
-output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.web_client.id
 }
 
-output "hosted_ui_domain" {
-  value = aws_cognito_user_pool_domain.this.domain
-}
-
 output "hosted_ui_login_url" {
-  value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${var.aws_region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.web_client.id}&response_type=code&scope=email+openid+profile&redirect_uri=http://localhost:3000/callback"
+  value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${var.aws_region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.web_client.id}&response_type=code&scope=openid+email+profile&redirect_uri=http://localhost:3000/callback"
 }
 
 output "hosted_ui_logout_url" {
